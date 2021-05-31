cf24b49033677b9905194654a99f94cf2d284767
Dev_Ose <osemum@hardcorebiometric.com>
2021-05-31 10:09:03 +0100

    Exploring dependent queries

--- src/Home.js
+++ src/Home.js
@@ -1,28 +1,57 @@
+import axios from 'axios';
 import React from 'react'
+import { useQuery } from 'react-query';
 import { useServerRequest } from './hooks/Hooks'
 
 
 function Home() {
-    
-    const { isLoading, error, data, isFetching } = useServerRequest('https://api.github.com/repos/tannerlinsley/react-query');
+    const email = 'Shanna@melissa.tv';
+    const userQuery = useServerRequest('users', 'https://jsonplaceholder.typicode.com/users?email='+email);
+    const postQuery = useQuery(
+        'posts', 
+        () =>
+        axios.get('https://jsonplaceholder.typicode.com/posts?userId='+userQuery.data?.id).then(res =>
+        res.data),
+        {
+            enabled: !!userQuery.data?.id
+        }
+    )
 
-  if (isLoading) return 'Loading...'
 
-  if (error) return 'An error has occurred: ' + error.message
+//   if (isLoading) return 'Loading...'
 
-  return (
-    <div>
-        {isFetching && 'Updating...'}
-        
-        
-      <h1>{data.name}</h1>
-      <p>{data.description}</p>
-      <strong> {data.subscribers_count}</strong>{' '}
-      <strong>{data.stargazers_count}</strong>{' '}
-      <strong>{data.forks_count}</strong>
-      
-    </div>
-  )
+//   if (error) return 'An error has occurred: ' + error.message
+
+  return userQuery.isLoading ? (
+        <p>Loading</p>
+    )
+    :
+    userQuery.error ?
+    (
+        <p>Error Occured!</p>
+    )
+    :
+    (
+        <div>
+            <p>User: {userQuery.data.id}</p>
+            {postQuery.isIdle ? (
+                'loading'
+            ):
+            postQuery.isLoading ? (
+                'loading'
+            )
+            :
+            (
+                <div>
+                    post count {postQuery.data.length}
+                </div>
+            )
+            
+            }
+        </div>
+    )
+  
+
 }
 
 export default Home
--- src/hooks/Hooks.js
+++ src/hooks/Hooks.js
@@ -2,18 +2,20 @@ import { useQuery } from 'react-query';
 import axios from 'axios';
 
 
-export function useServerRequest(url){
+export function useServerRequest(keyVal, url, enabledKey){
     const { isLoading, error, data, isFetching } = useQuery(
-        'repoData', 
+        keyVal, 
         () =>
         axios.get(url).then(res =>
-        res.data),
+        res.data[0]),
         {
             // refetchOnWindowFocus:false
             // staleTime: 10000 // used to determine when a data should become stale
             // cacheTime: 5000 //used to determine the time a data should be cached
             // enabled: key // query will work if the value defined here is equal to the useQuery key value 
             // retry: 2 // number of times a query should retry if it fails
+            // retryDelay: 1000
+            enabled: enabledKey
         }
     )
 
